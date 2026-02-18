8058171809395f870e03a61e825f0396a99fbcee
Wilson Oliveira Lima <oliveiralima@protonmail.com>
2026-02-18 14:53:36 -0300

    chore: add generated SDKs for v1

--- src/v1/python/openapi_client/api/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.pedidos_api import PedidosApi
-from openapi_client.api.saques_e_transferncias_api import SaquesETransfernciasApi
-
